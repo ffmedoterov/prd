e2af1cc734005431f097b6f3bf45150aa191605a
ffmedoterov <92450024+ffmedoterov@users.noreply.github.com>
2025-03-05 18:20:51 +0400

    1.lua

--- 1.lua
+++ 1.lua
@@ -1,3 +1,55 @@
+
+
+local menu_refs = {
+    ["aimbot"] = ui.reference("RAGE", "Aimbot", "Enabled"),
+    ["doubletap"] = { ui.reference("RAGE", "Aimbot", "Double tap") },
+    ["hideshots"] = { ui.reference("AA", "Other", "On shot anti-aim") }
+}
+
+local timer = globals.tickcount()
+local scriptleakstop = 14
+
+local ctx = (function()
+    local ctx = {}
+
+    ctx.recharge = {
+        run = function()
+            local lp = entity.get_local_player()
+
+            if not entity.is_alive(lp) then return end
+
+            local lp_weapon = entity.get_player_weapon(lp)
+            if not lp_weapon then return end
+
+            scriptleakstop = weapons(lp_weapon).is_revolver and 17 or 14
+ 
+            if ui.get(menu_refs["doubletap"][2]) or ui.get(menu_refs["hideshots"][2]) then
+                if globals.tickcount() >= timer + scriptleakstop then
+                    ui.set(menu_refs["aimbot"], true)
+                else
+                    ui.set(menu_refs["aimbot"], false)
+                end
+            else
+                timer = globals.tickcount()
+                ui.set(menu_refs["aimbot"], true)
+            end
+        end
+    }
+
+    return ctx
+end)()
+
+client.set_event_callback('setup_command', function(cmd)
+    ctx.recharge.run()
+end)
+
+client.set_event_callback('level_init', function()
+    timer = globals.tickcount()
+end)
+
+ 
+
+
 ------@libs
 local ffi = require("ffi")
 local c_entity = require("gamesense/entity")
@@ -8,7 +60,7 @@ local base64 = require("gamesense/base64")
 local clipboard = require("gamesense/clipboard")
 local vector = require("vector")
 local c_entity = require('gamesense/entity')
-local antiaim_funcs = require("gamesense/antiaim_funcs")
+local weapons = require("gamesense/csgo_weapons")
 local json = require("json")
 ------@libs
 
@@ -1139,6 +1191,15 @@ mapname = globals.mapname()
 }
 
 ------@ref
+
+local ref = {
+    aimbot = ui.reference('RAGE', 'Aimbot', 'Enabled'),
+    doubletap = {
+        main = {ui.reference('RAGE', 'Aimbot', 'Double tap')},
+        fakelag_limit = ui.reference('RAGE', 'Aimbot', 'Double tap fake lag limit')
+    },
+    onshot_antiaim = {ui.reference('AA', 'Other', 'On shot anti-aim')}
+}
 
 gamesense_refs.prefer_safe_point = ui.reference('RAGE', 'Aimbot', 'Prefer safe point')
 gamesense_refs.force_safe_point = ui.reference('RAGE', 'Aimbot', 'Force safe point')
@@ -3305,124 +3366,30 @@ initDatabase2()
         self_anim_overlay.weight = 0
 end
 end
-------XDDDDDDDDDDDDDDDD
-local vars = {
-    ref = {
-        aimbot = ui.reference("RAGE", "Aimbot", "Enabled"),
-
-        dt = ui.reference("RAGE", "Aimbot", "Double tap"),
-        dt_key = select(2, ui.reference("RAGE", "Aimbot", "Double tap")),
-
-        flags = ui.reference("VISUALS", "Player ESP", "Flags"),
-    },
-    globals = {
-        local_vulnerable = nil,
-        tickbase = nil,
-        
-        charged = false,
-
-        data = {},
-        index = 1,
-    },
-}
-
-local func = {
-    update_vulnerable_state = function(local_player)
-        local th = client.current_threat()
-
-        if th == nil then 
-            vars.globals.local_vulnerable = false 
-            return 
-        end
-
-        if ui.get(vars.ref.flags) then 
-            vars.globals.local_vulnerable = (bit.band(entity.get_esp_data(th).flags, bit.lshift(1, 11)) == 2048) 
-            return
-        else
-            if entity.is_dormant(th) then 
-                vars.globals.local_vulnerable = false 
-                return 
-            end
-
-            local start_pos = {entity.hitbox_position(th, 0)}
-            local end_pos = {entity.get_prop(local_player, "m_vecOrigin")}
-            end_pos[3] = end_pos[3] + 32
-
-            local _, dmg = client.trace_bullet(th, start_pos[1], start_pos[2], start_pos[3], end_pos[1], end_pos[2], end_pos[3], false)
-
-            vars.globals.local_vulnerable = dmg > 1
-        end
-    end,
-    weapon_can_fire = function(ent)
-	    local active_weapon = entity.get_prop(ent, "m_hActiveWeapon")
-	    local nextAttack = entity.get_prop(active_weapon, "m_flNextPrimaryAttack")
-	    return globals.curtime() >= nextAttack
-    end,
-}
-
-client.set_event_callback('net_update_end', function()
-    local local_player = entity.get_local_player()
-    if local_player == nil then return end 
-
-    if vars.globals.tickbase == nil then
-        vars.globals.tickbase = entity.get_prop(local_player, 'm_nTickBase')
+client.set_event_callback('setup_command', function()
+    if not ((ui.get(ref.doubletap.main[1]) and ui.get(ref.doubletap.main[2])) or (ui.get(ref.onshot_antiaim[1]) and ui.get(ref.onshot_antiaim[2]))) then
+        ui.set(ref.aimbot, true)
         return
     end
 
-    local current_tickbase = entity.get_prop(local_player, 'm_nTickBase')
+    local m_nTickBase = entity.get_prop(entity.get_local_player(), 'm_nTickBase')
+    local shift = math.floor(m_nTickBase - globals.tickcount() - 3 - toticks(client.latency()) * .4)
+    local ticks_charged = shift <= -15 + (ui.get(ref.doubletap.fakelag_limit) - 1) + 5
 
-    vars.globals.data[vars.globals.index] = current_tickbase - vars.globals.tickbase
-    vars.globals.index = vars.globals.index + 1
-    vars.globals.index = vars.globals.index % 16
+    local threat = client.current_threat()
 
-    vars.globals.charged = false 
-
-    for i=1, 15 do 
-       if vars.globals.data[i] ~= nil and vars.globals.data[i] < 0 then
-            vars.globals.charged = true
-            return
-       end
+    if ticks_charged
+    or not threat
+    or bit.band(entity.get_esp_data(threat).flags, bit.lshift(1, 11)) ~= 2048 then
+        ui.set(ref.aimbot, true)
+        return
     end
 
-    if vars.globals.charged == false and antiaim_funcs.get_tickbase_shifting() > 0 then
-        vars.globals.charged = true
-    end
-
-    vars.globals.tickbase = current_tickbase
+    ui.set(ref.aimbot, false)
 end)
 
-client.set_event_callback("setup_command", function(e)
-    local local_player = entity.get_local_player()
-    if local_player == nil then return end 
-
-    if bit.band(entity.get_prop(local_player, "m_fFlags"), 1) == 1 then 
-        ui.set(vars.ref.aimbot, true) 
-        return 
-    end
-
-    func.update_vulnerable_state(local_player)
-        
-    if not ui.get(vars.ref.dt) or not ui.get(vars.ref.dt_key) or vars.globals.local_vulnerable == false then
-        ui.set(vars.ref.aimbot, true) 
-        return 
-    end
-    
-    local weapon = entity.get_player_weapon(local_player)
-
-    if weapon == nil then 
-        ui.set(vars.ref.aimbot, true) 
-        return 
-    end
-
-    if func.weapon_can_fire(local_player) and entity.get_classname(weapon) == 'CKnife' then 
-        ui.set(vars.ref.aimbot, true)
-    end
-    
-    ui.set(vars.ref.aimbot, vars.globals.charged) 
-end)
- 
-client.set_event_callback("shutdown", function()
-    ui.set(vars.ref.aimbot, true) 
+client.set_event_callback('shutdown', function()
+    ui.set(ref.aimbot, true)
 end)
 end)
 
